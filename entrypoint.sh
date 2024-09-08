c64be596b482119c05df338058442efd74261f1d
seeleng <seelengxd@gmail.com>
2024-09-08 11:32:53 +0800

    refactor: cleanup code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ pip3 install -r requirements.txt
 # fixes cursed problem
 # from .exceptions import InvalidKeyError ModuleNotFoundError: No module named 'jwt.exceptions'
 # somehow the installation order matters
-
 pip3 uninstall -y pyjwt jwt 
 pip3 install jwt
 pip3 install pyjwt
@@ -15,5 +14,3 @@ python3 -m src.unicon_backend.utils.seed
 
 # run CMD command 
 exec "$@"
-
-
