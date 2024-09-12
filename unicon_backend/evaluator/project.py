ada24123cfd89b8b372a6e3282ec22df4b64d87d
Shen Yi Hong <shen_yi_hong@outlook.com>
2024-09-12 18:45:18 +0800

    feat: subclass serialization using `SerializeAsAny`

--- unicon_backend/evaluator/project.py
+++ unicon_backend/evaluator/project.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, SerializeAsAny
 
 from unicon_backend.evaluator.tasks.base import Task
 
@@ -6,4 +6,4 @@ from unicon_backend.evaluator.tasks.base import Task
 class Project(BaseModel):
     name: str
     description: str
-    tasks: list[Task]
+    tasks: list[SerializeAsAny[Task]]
