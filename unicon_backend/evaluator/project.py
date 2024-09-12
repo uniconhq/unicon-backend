252e8c59df8939722691f0f706b1e1b3c862ad02
seeleng <seelengxd@gmail.com>
2024-09-12 20:14:56 +0800

    feat: init jinja for answers
    - adds test_answer.json and Answer model
    - creates an output file
    - for now generates a random uuid for run for each programming task
    - for each testcase a folder
    - so output/random_uuid/test_case_id/run.py is combining the answer with
      function run step

--- unicon_backend/evaluator/project.py
+++ unicon_backend/evaluator/project.py
@@ -1,9 +1,16 @@
 from pydantic import BaseModel, SerializeAsAny
 
 from unicon_backend.evaluator.tasks.base import Task
+from unicon_backend.evaluator.tasks.programming import ProgrammingTask
+from unicon_backend.evaluator.answer import Answer
 
 
 class Project(BaseModel):
     name: str
     description: str
     tasks: list[SerializeAsAny[Task]]
+
+    def run(self, answer: Answer):
+        for task in self.tasks:
+            if isinstance(task, ProgrammingTask):
+                task.run(answer)
--- unicon_backend/evaluator/tasks/programming.py
+++ unicon_backend/evaluator/tasks/programming.py
@@ -1,9 +1,17 @@
+import abc
 from enum import Enum
-from typing import Any
+import os
+from typing import Any, TYPE_CHECKING, Generic
+from uuid import uuid4
 
 from pydantic import BaseModel
 
-from unicon_backend.evaluator.tasks.base import Task
+from unicon_backend.evaluator.tasks.base import InputType, OutputType, Task
+from unicon_backend.lib.common import CustomBaseModel
+from unicon_backend.templates import template
+
+if TYPE_CHECKING:
+    from unicon_backend.evaluator.answer import Answer
 
 
 class ProgrammingLanguage(str, Enum):
@@ -36,10 +44,26 @@ class PyRunFunctionStep(Step[Any, Any]):
     arguments: list[str | int]
     keyword_arguments: dict[str, str]
 
+    def get_code(self):
+        pass_to_function = self.arguments + \
+            [f"{key}={value}" for key, value in self.keyword_arguments.items()]
+        return f"{self.function_name}({', '.join(map(str, pass_to_function))})"
+
 
 class Testcase(BaseModel):
     id: int
     steps: list[Step]
+
+    def run(self, prefix: str, answer: "Answer"):
+        folder_path = os.path.join("output", prefix, str(self.id))
+        os.mkdir(folder_path)
+        artifacts = answer.artifacts
+        run_funcs = [step for step in self.steps if isinstance(
+            step, PyRunFunctionStep)]
+        file = template.render(
+            prepend="", artifacts=artifacts, run_funcs=run_funcs)
+        with open(os.path.join(folder_path, "run.py"), "w") as f:
+            f.write(file)
 
 
 class ProgrammingTask(Task[Any, Any]):
@@ -50,5 +74,10 @@ class ProgrammingTask(Task[Any, Any]):
 
     input: list[File]
 
-    def run(self, expected: Any) -> Any:
-        raise NotImplementedError()
+    def run(self, answer: "Answer") -> bool:
+        prefix = str(uuid4())
+        if "output" not in os.listdir():
+            os.mkdir("output")
+        os.mkdir(os.path.join("output", prefix))
+        for testcase in self.testcases:
+            testcase.run(prefix, answer)
