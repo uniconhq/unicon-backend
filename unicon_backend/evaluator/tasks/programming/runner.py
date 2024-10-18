99ee6047b866dddeb521401a5f8dbb87d20b0eda
Shen Yi Hong <shen_yi_hong@outlook.com>
2024-10-18 16:19:20 +0800

    feat: new runner abstraction to handle running assembled programs

---
+++ unicon_backend/evaluator/tasks/programming/runner.py
@@ -0,0 +1,59 @@
+from enum import Enum
+from typing import Any, NewType, Self
+from uuid import UUID
+
+from pydantic import BaseModel, model_validator
+
+from unicon_backend.evaluator.tasks.programming.artifact import File
+
+
+class RunnerType(str, Enum):
+    PODMAN = "podman"
+    SANDBOX = "sandbox"
+    UNSAFE = "unsafe"
+
+
+SubmissionId = NewType("SubmissionId", UUID)
+
+
+class ProgrammingLanguage(str, Enum):
+    PYTHON = "PYTHON"
+
+
+class RunnerEnvironment(BaseModel):
+    language: ProgrammingLanguage
+    time_limit: int  # in seconds
+    memory_limit: int  # in MB
+
+    extra_options: dict[str, Any]
+
+
+class RunnerResponse(BaseModel):
+    submission_id: SubmissionId
+    status: int
+    stdout: str
+    stderr: str
+
+
+class RunnerPackage(BaseModel):
+    entrypoint: str
+    files: list[File]
+
+    @model_validator(mode="after")
+    def check_entrypoint_exists_in_files(self) -> Self:
+        if not any(file.file_name == self.entrypoint for file in self.files):
+            raise ValueError(f"Entrypoint {self.entrypoint} not found in RunnerPackage files")
+        return self
+
+
+class RunnerRequest(BaseModel):
+    submission_id: SubmissionId
+    package: RunnerPackage
+
+    environment: RunnerEnvironment
+
+    @staticmethod
+    def create(entrypoint: str, files: list[File], environment: RunnerEnvironment) -> SubmissionId:
+        package = RunnerPackage(entrypoint=entrypoint, files=files)
+        submission_id = SubmissionId(UUID())
+        return RunnerRequest(submission_id=submission_id, package=package, environment=environment)
