31c0bd4bf02a5f9838803c81785d86583381dab6
Shen Yi Hong <shen_yi_hong@outlook.com>
2024-10-19 14:23:46 +0800

    fix: toposort using wrong edge index

--- unicon_backend/lib/graph.py
+++ unicon_backend/lib/graph.py
@@ -39,24 +39,40 @@ class Graph(BaseModel, Generic[GraphNodeType]):
         return {node.id: node for node in self.nodes}
 
     @cached_property
+    def out_nodes_index(self) -> defaultdict[int, list[int]]:
+        """Return a dictionary of node id to a list of ids of nodes that are outgoing from the node"""
+        out_nodes_index = defaultdict(list)
+        for edge in self.edges:
+            out_nodes_index[edge.from_node_id].append(edge.to_node_id)
+        return out_nodes_index
+
+    @cached_property
+    def in_nodes_index(self) -> defaultdict[int, list[int]]:
+        """Return a dictionary of node id to a list of ids of nodes that are incoming to the node"""
+        in_nodes_index = defaultdict(list)
+        for edge in self.edges:
+            in_nodes_index[edge.to_node_id].append(edge.from_node_id)
+        return in_nodes_index
+
+    @cached_property
     def edge_index(self) -> dict[int, GraphEdge]:
         """Return a dictionary of edge id to edge object"""
         return {edge.id: edge for edge in self.edges}
 
     @cached_property
     def out_edges_index(self) -> defaultdict[int, list[int]]:
-        """Return a dictionary of node id to a list of node ids that are connected by an edge from the node"""
+        """Return a dictionary of node id to a list of ids of edges that are outgoing from the node"""
         out_edges_index = defaultdict(list)
         for edge in self.edges:
-            out_edges_index[edge.from_node_id].append(edge.to_node_id)
+            out_edges_index[edge.from_node_id].append(edge.id)
         return out_edges_index
 
     @cached_property
     def in_edges_index(self) -> defaultdict[int, list[int]]:
-        """Return a dictionary of node id to a list of node ids that are connected by an edge to the node"""
+        """Return a dictionary of node id to a list of ids of edges that are incoming to the node"""
         in_edges_index = defaultdict(list)
         for edge in self.edges:
-            in_edges_index[edge.to_node_id].append(edge.from_node_id)
+            in_edges_index[edge.to_node_id].append(edge.id)
         return in_edges_index
 
     def topological_sort(self) -> list[GraphNodeType]:
@@ -73,7 +89,7 @@ class Graph(BaseModel, Generic[GraphNodeType]):
         node_id_queue: deque[int] = deque(maxlen=len(self.nodes))
 
         for node in self.nodes:
-            in_degrees[node.id] = len(self.in_edges_index.get(node.id, []))
+            in_degrees[node.id] = len(self.in_nodes_index.get(node.id, []))
             if in_degrees[node.id] == 0:
                 node_id_queue.append(node.id)
 
@@ -82,7 +98,7 @@ class Graph(BaseModel, Generic[GraphNodeType]):
             step_node_id: int = node_id_queue.popleft()
             topo_order.append(step_node_id)
 
-            for to_step_node_id in self.out_edges_index.get(step_node_id, []):
+            for to_step_node_id in self.out_nodes_index.get(step_node_id, []):
                 in_degrees[to_step_node_id] -= 1
                 if in_degrees[to_step_node_id] == 0:
                     node_id_queue.append(to_step_node_id)
