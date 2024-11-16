cb4b6c58d5061375e4c7eb3b5f39e79f72e0ae42
seeleng <seelengxd@gmail.com>
2024-11-16 16:34:13 +0800

    fix: assign pyrunstep output to data variable

--- unicon_backend/evaluator/tasks/programming/steps.py
+++ unicon_backend/evaluator/tasks/programming/steps.py
@@ -388,6 +388,24 @@ class PyRunFunctionStep(Step):
 
     function_identifier: str
 
+    @model_validator(mode="after")
+    def check_has_exactly_one_data_output(self) -> Self:
+        if (
+            num_data_inputs := len(
+                [out_socket for out_socket in self.outputs if out_socket.type == "DATA"]
+            )
+        ) != 1:
+            raise ValueError(
+                f"Py run function step ({self.id}) must have exactly one data output, found {num_data_inputs}"
+            )
+
+        if "DATA.OUT" not in self.out_socket_index:
+            raise ValueError(
+                f"Py run function step ({self.id}) must have a data output socket with the id DATA.OUT"
+            )
+
+        return self
+
     def run(
         self,
         var_inputs: dict[SocketName, ProgramVariable],
@@ -426,14 +444,14 @@ class PyRunFunctionStep(Step):
             *self.debug_stmts(),
             *(
                 [
-                    f"{self.get_output_variable(self.outputs[0].id)} = call_function_safe('{module_name}', '{self.function_identifier}', {function_args_str})"
+                    f"{self.get_output_variable(self.out_socket_index["DATA.OUT"].id)} = call_function_safe('{module_name}', '{self.function_identifier}', {function_args_str})"
                 ]
                 if untrusted
                 else [
                     # Import statement for the function
                     f"from {module_name} import {self.function_identifier}",
                     # Function invocation
-                    f"{self.get_output_variable(self.outputs[0].id)} = {self.function_identifier}({function_args_str})",
+                    f"{self.get_output_variable(self.out_socket_index["DATA.OUT"].id)} = {self.function_identifier}({function_args_str})",
                 ]
             ),
         ]
