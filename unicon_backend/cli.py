201115714279895d890dd0dd5d2d5952b903ac5c
seeleng <seelengxd@gmail.com>
2025-02-02 20:02:59 +0800

    refactor(permissions): use importlib for init

--- unicon_backend/cli.py
+++ unicon_backend/cli.py
@@ -1,3 +1,4 @@
+import importlib.resources
 from typing import Annotated
 
 import typer
@@ -23,7 +24,13 @@ app.add_typer(permify_app, name="permify")
 @permify_app.command(name="init")
 def init_permify():
     """Sends the schema file to permify."""
-    schema_version = init_schema("unicon_backend/lib/permissions/unicon.perm")
+
+    schema = (
+        importlib.resources.files("unicon_backend.lib.permissions")
+        .joinpath("unicon.perm")
+        .read_text()
+    )
+    schema_version = init_schema(schema)
     print(f"Schema version: {schema_version}. Please update your .env file.")
 
 
