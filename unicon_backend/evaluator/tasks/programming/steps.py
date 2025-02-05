0e7be2f06aecacda65dd956de82ba004ac83ae5a
Shen Yi Hong <shen_yi_hong@outlook.com>
2025-02-05 16:51:52 +0800

    refactor: link variables

--- unicon_backend/evaluator/tasks/programming/steps.py
+++ unicon_backend/evaluator/tasks/programming/steps.py
@@ -4,10 +4,10 @@ from collections import deque
 from collections.abc import MutableSequence, Sequence
 from enum import Enum, StrEnum
 from functools import cached_property
-from typing import TYPE_CHECKING, Any, ClassVar, Self
+from typing import TYPE_CHECKING, Any, ClassVar, Self, cast
 
 import libcst as cst
-from pydantic import model_validator
+from pydantic import PrivateAttr, model_validator
 
 from unicon_backend.evaluator.tasks.programming.artifact import File, PrimitiveData
 from unicon_backend.evaluator.tasks.programming.transforms import hoist_imports
@@ -191,15 +191,12 @@ class Step[SocketT: StepSocket](
 
         return subgraph_node_ids
 
-    def get_output_variable(self, output: SocketId) -> ProgramVariable:
-        return cst.Name(f"var_{self.id}_{output}".replace(".", "_"))
-
     @abc.abstractmethod
     def run(
         self,
-        var_inputs: dict[SocketId, ProgramVariable],
-        file_inputs: dict[SocketId, File],
         graph: "ComputeGraph",
+        in_vars: dict[SocketId, ProgramVariable],
+        in_files: dict[SocketId, File],
     ) -> ProgramFragment: ...
 
 
@@ -210,35 +207,28 @@ class InputStep(Step[StepSocket]):
 
     @model_validator(mode="after")
     def check_non_empty_data_outputs(self) -> Self:
-        if not self.is_user and (
-            empty_socket_ids := [socket.id for socket in self.data_out if socket.data is None]
-        ):
+        if empty_socket_ids := [socket.id for socket in self.data_out if socket.data is None]:
             raise ValueError(f"Missing data for output sockets {','.join(empty_socket_ids)}")
         return self
 
-    def run(self, *_) -> ProgramFragment:
+    def run(self, graph: "ComputeGraph", *_) -> ProgramFragment:
         def _parse(data: str | int | float | bool) -> cst.BaseExpression:
-            # TODO: Better handle of variables vs strings
-            data_repr: str = (
+            return cst.parse_expression(
                 repr(data)
                 if not isinstance(data, str)
-                else (f'"{data}"' if not data.startswith("var_") else data)
+                else (f'"{data}"' if not data.startswith(graph.VAR_PREFIX) else data)
             )
-            return cst.parse_expression(data_repr)
 
         program = []
-        for socket in self.data_out:
-            if isinstance(socket.data, File):
-                # If the input is a `File`, we skip the serialization and just pass the file object
-                # directly to the next step. This is handled by the `ComputeGraph` class
-                continue
-            elif isinstance(socket.data, PrimitiveData):
-                program.append(
-                    cst.Assign(
-                        targets=[cst.AssignTarget(self.get_output_variable(socket.id))],
-                        value=_parse(socket.data),
-                    )
+        # If the input is a `File`, we skip the serialization and just pass the file object
+        # directly to the next step. This is handled by the `ComputeGraph` class
+        for socket in filter(lambda s: isinstance(s.data, PrimitiveData), self.data_out):
+            program.append(
+                cst.Assign(
+                    targets=[cst.AssignTarget(graph.get_link_var(self, socket))],
+                    value=_parse(cast(PrimitiveData, socket.data)),
                 )
+            )
 
         return program
 
@@ -289,14 +279,15 @@ class OutputSocket(StepSocket):
 class OutputStep(Step[OutputSocket]):
     required_data_io: ClassVar[tuple[Range, Range]] = ((1, -1), (0, 0))
 
-    def run(self, var_inputs: dict[SocketId, ProgramVariable], *_) -> ProgramFragment:
+    def run(
+        self, _graph: "ComputeGraph", in_vars: dict[SocketId, ProgramVariable], *_
+    ) -> ProgramFragment:
         result_dict = cst.Dict(
             [
-                cst.DictElement(key=cst.SimpleString(repr(socket.id)), value=var_inputs[socket.id])
+                cst.DictElement(key=cst.SimpleString(socket.label), value=in_vars[socket.id])
                 for socket in self.data_in
             ]
         )
-
         return [
             cst.Import([cst.ImportAlias(name=cst.Name("json"))]),
             cst.Expr(
@@ -318,19 +309,18 @@ class OutputStep(Step[OutputSocket]):
 class StringMatchStep(Step[StepSocket]):
     required_data_io: ClassVar[tuple[Range, Range]] = ((2, 2), (1, 1))
 
-    def run(self, var_inputs: dict[SocketId, ProgramVariable], *_) -> ProgramFragment:
+    def run(
+        self, graph: "ComputeGraph", in_vars: dict[SocketId, ProgramVariable], *_
+    ) -> ProgramFragment:
+        match_result_socket, [match_op_1, match_op_2] = self.data_out[0], self.data_in
+        str_cast = lambda var: cst.Call(cst.Name("str"), args=[cst.Arg(var)])
         return [
             cst.Assign(
-                targets=[cst.AssignTarget(self.get_output_variable(self.outputs[0].id))],
+                targets=[cst.AssignTarget(graph.get_link_var(self, match_result_socket))],
                 value=cst.Comparison(
-                    left=cst.Call(cst.Name("str"), args=[cst.Arg(var_inputs[self.data_in[0].id])]),
+                    left=str_cast(in_vars[match_op_1.id]),
                     comparisons=[
-                        cst.ComparisonTarget(
-                            cst.Equal(),
-                            cst.Call(
-                                cst.Name("str"), args=[cst.Arg(var_inputs[self.data_in[1].id])]
-                            ),
-                        )
+                        cst.ComparisonTarget(cst.Equal(), str_cast(in_vars[match_op_2.id]))
                     ],
                 ),
             )
@@ -347,12 +337,14 @@ class ObjectAccessStep(Step[StepSocket]):
 
     key: str
 
-    def run(self, var_inputs: dict[SocketId, ProgramVariable], *_) -> ProgramFragment:
+    def run(
+        self, graph: "ComputeGraph", in_vars: dict[SocketId, ProgramVariable], *_
+    ) -> ProgramFragment:
         return [
             cst.Assign(
-                targets=[cst.AssignTarget(self.get_output_variable(self.data_out[0].id))],
+                targets=[cst.AssignTarget(graph.get_link_var(self, self.data_out[0]))],
                 value=cst.Subscript(
-                    value=var_inputs[self.data_in[0].id],
+                    value=in_vars[self.data_in[0].id],
                     slice=[cst.SubscriptElement(cst.Index(cst.SimpleString(repr(self.key))))],
                 ),
             )
@@ -360,28 +352,17 @@ class ObjectAccessStep(Step[StepSocket]):
 
 
 class PyRunFunctionStep(Step[StepSocket]):
-    """
-    A step that runs a Python function.
-    To use this step, the user must provide the function name and the arguments to the function via the input sockets.
-
-    Socket Name Format:
-    - DATA.IN.ARG.{index}.{name}: For positional arguments
-    - DATA.IN.KWARG.{name}: For keyword arguments
-    - DATA.IN.FILE: For the `File` object that contains the Python function
-    """
-
     required_data_io: ClassVar[tuple[Range, Range]] = ((1, -1), (1, 2))
 
-    _data_in_file_id: ClassVar[str] = "DATA.IN.FILE"
+    _file_socket_alias: ClassVar[str] = "DATA.IN.FILE"
+    _error_socket_alias: ClassVar[str] = "DATA.OUT.ERROR"
 
     function_identifier: str
-
     allow_error: bool = False
-    _data_out_error_id: ClassVar[str] = "DATA.OUT.ERROR"
 
     @model_validator(mode="after")
     def check_module_file_input(self) -> Self:
-        if not any(socket.label == "FILE" for socket in self.data_in):
+        if not any(socket.alias == self._file_socket_alias for socket in self.data_in):
             raise ValueError("No module file input provided")
         return self
 
@@ -395,15 +376,11 @@ class PyRunFunctionStep(Step[StepSocket]):
                 f"Expected {expected_sockets} output socket(s) when `allow_error` is {self.allow_error}"
             )
 
-        if self.allow_error and self._data_out_error_id not in [
-            socket.id for socket in self.data_out
-        ]:
-            raise ValueError(f"Missing error output socket {self._data_out_error_id}")
-
-        if not self.allow_error and self._data_out_error_id in [
-            socket.id for socket in self.data_out
-        ]:
-            raise ValueError(f"Unexpected error output socket {self._data_out_error_id}")
+        has_error_socket = any(socket.alias == self._error_socket_alias for socket in self.data_out)
+        if self.allow_error and not has_error_socket:
+            raise ValueError(f"Missing error output socket {self._error_socket_alias}")
+        if not self.allow_error and has_error_socket:
+            raise ValueError(f"Unexpected error output socket {self._error_socket_alias}")
 
         return self
 
@@ -418,49 +395,45 @@ class PyRunFunctionStep(Step[StepSocket]):
 
     def run(
         self,
-        var_inputs: dict[SocketId, ProgramVariable],
-        file_inputs: dict[SocketId, File],
         graph: "ComputeGraph",
+        in_vars: dict[SocketId, ProgramVariable],
+        in_files: dict[SocketId, File],
     ) -> ProgramFragment:
         # Get the input file that we are running the function from
-        program_file: File | None = file_inputs.get(self._data_in_file_id)
+        file_socket_id = self.alias_map[self._file_socket_alias].id
+        program_file: File | None = in_files.get(file_socket_id)
         if program_file is None:
             raise ValueError("No program file provided")
 
         # NOTE: Assume that there can only be one edge connected to the file input socket. This should ideally be validated.
-        # NOTE: Assume that the user-provided input is always stored in the `Step` with id = 0
-        is_user_provided_file: bool = [
-            edge
+        from_file_node_id = [
+            edge.from_node_id
             for edge in graph.in_edges_index[self.id]
-            if edge.to_socket_id == self._data_in_file_id
-        ][0].from_node_id == 0
+            if edge.to_socket_id == file_socket_id
+        ][0]
+        from_file_node = cast(InputStep, graph.node_index[from_file_node_id])
+        is_user_provided_file: bool = from_file_node.is_user
 
         # NOTE: Assume that the program file is always a Python file
         module_name_str = program_file.name.split(".py")[0]
 
         func_name = cst.Name(self.function_identifier)
-        args = [cst.Arg(var_inputs[socket.id]) for socket in self.arg_sockets]
+        args = [cst.Arg(in_vars[socket.id]) for socket in self.arg_sockets]
         kwargs = [
-            cst.Arg(var_inputs[socket.id], keyword=cst.Name(socket.label.split(".", 1)[1]))
+            cst.Arg(in_vars[socket.id], keyword=cst.Name(socket.label.split(".", 1)[1]))
             for socket in self.kwarg_sockets
         ]
 
-        non_error_sockets = [
-            socket for socket in self.data_out if socket.id != self._data_in_file_id
-        ]
-        output_var_name = self.get_output_variable(non_error_sockets[0].id)
-        error_var_name = (
-            self.get_output_variable(self._data_out_error_id) if self.allow_error else cst.Name("_")
-        )
+        error_socket = self.alias_map[self._error_socket_alias]
+        non_error_sockets = [socket for socket in self.data_out if socket.id != error_socket.id]
+
+        out_var = graph.get_link_var(self, non_error_sockets[0])
+        error_var = graph.get_link_var(self, error_socket) if self.allow_error else cst.Name("_")
 
         return (
             [
                 cst.Assign(
-                    [
-                        cst.AssignTarget(
-                            cst.Tuple([cst.Element(output_var_name), cst.Element(error_var_name)])
-                        )
-                    ],
+                    [cst.AssignTarget(cst.Tuple([cst.Element(out_var), cst.Element(error_var)]))],
                     cst.Call(
                         cst.Name("call_function_safe"),
                         [
@@ -476,7 +449,7 @@ class PyRunFunctionStep(Step[StepSocket]):
             if is_user_provided_file
             else [
                 cst.ImportFrom(cst.Name(module_name_str), [cst.ImportAlias(func_name)]),
-                cst.Assign([cst.AssignTarget(output_var_name)], cst.Call(func_name, args + kwargs)),
+                cst.Assign([cst.AssignTarget(out_var)], cst.Call(func_name, args + kwargs)),
             ]
         )
 
@@ -490,7 +463,7 @@ class LoopStep(Step[StepSocket]):
     required_data_io: ClassVar[tuple[Range, Range]] = ((0, 0), (0, 0))
 
     def run(
-        self, var_inputs: dict[SocketId, ProgramVariable], _, graph: "ComputeGraph"
+        self, graph: "ComputeGraph", in_vars: dict[SocketId, ProgramVariable], *_
     ) -> ProgramFragment:
         return [
             cst.While(
@@ -499,7 +472,7 @@ class LoopStep(Step[StepSocket]):
                     [
                         *self.run_subgraph(self._pred_socket_alias, graph).body,
                         cst.If(
-                            test=var_inputs[self.alias_map[self._pred_socket_alias].id],
+                            test=in_vars[self.alias_map[self._pred_socket_alias].id],
                             body=cst.SimpleStatementSuite([cst.Break()]),
                         ),
                         *self.run_subgraph(self._body_socket_alias, graph).body,
@@ -523,12 +496,12 @@ class IfElseStep(Step[StepSocket]):
     required_data_io: ClassVar[tuple[Range, Range]] = ((0, 0), (0, 0))
 
     def run(
-        self, var_inputs: dict[SocketId, ProgramVariable], _, graph: "ComputeGraph"
+        self, graph: "ComputeGraph", in_vars: dict[SocketId, ProgramVariable], *_
     ) -> ProgramFragment:
         return [
             *self.run_subgraph(self._pred_socket_alias, graph).body,
             cst.If(
-                test=var_inputs[self.alias_map[self._pred_socket_alias].id],
+                test=in_vars[self.alias_map[self._pred_socket_alias].id],
                 body=cst.IndentedBlock([*self.run_subgraph(self._if_socket_alias, graph).body]),
                 orelse=cst.Else(
                     cst.IndentedBlock([*self.run_subgraph(self._else_socket_alias, graph).body])
@@ -549,15 +522,18 @@ StepClasses = (
 
 
 class ComputeGraph(Graph[StepClasses, GraphEdge[str]]):  # type: ignore
-    def _create_link_variable(self, from_node: Step, from_socket: str) -> ProgramVariable:
-        """
-        Create a variable name for the output of a node. The variable name must be unique across all nodes and sockets.
+    VAR_PREFIX: ClassVar[str] = "var_"
+    _var_id: int = PrivateAttr(default=0)
 
-        Args:
-            from_node (Step): The node that is outputting the variable
-            from_socket (str): The socket that the variable is outputting from
-        """
-        return from_node.get_output_variable(from_socket)
+    def _get_uniq_var_id(self) -> int:
+        ret_var_id = self._var_id
+        self._var_id += 1
+        return ret_var_id
+
+    def get_link_var(self, from_node: Step, from_socket: StepSocket) -> ProgramVariable:
+        return cst.Name(
+            f"{self.VAR_PREFIX}{self._get_uniq_var_id()}_{from_node.type.value}_{from_socket.label}"
+        )
 
     def link_type(self, edge: GraphEdge[str]) -> SocketType:
         def get_step_socket(step_id: str, socket_id: str) -> StepSocket | None:
@@ -618,9 +594,9 @@ class ComputeGraph(Graph[StepClasses, GraphEdge[str]]):  # type: ignore
                         # NOTE: File objects are passed directly to the next step and not serialized as a variable
                         in_files[to_s.id] = from_s.data
                     else:
-                        in_vars[to_s.id] = self._create_link_variable(from_n, from_s.id)
+                        in_vars[to_s.id] = self.get_link_var(from_n, from_s)
 
             node._debug = debug
-            program_body.extend(assemble_fragment(node.run(in_vars, in_files, self)))
+            program_body.extend(assemble_fragment(node.run(self, in_vars, in_files)))
 
         return hoist_imports(cst.Module(body=program_body))
