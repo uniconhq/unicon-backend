e28cc304b0627d3e541d0cc95b82321bc56c6dac
seeleng <seelengxd@gmail.com>
2025-02-08 21:27:57 +0800

    refactor: remove schema/__init__.py
    model mysteriously can build correctly now

--- unicon_backend/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from unicon_backend.schemas.auth import UserPublic
-from unicon_backend.schemas.organisation import OrganisationPublicWithMembers
-
-OrganisationPublicWithMembers.model_rebuild()
-
-__all__ = [
-    "UserPublic",
-    "OrganisationPublicWithMembers",
-]
